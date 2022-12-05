d54ae3b004c0dfae57e5364ca73bd152e5d4c6eb
levitatingmoon <94081701+levitatingmoon@users.noreply.github.com>
2022-12-05 17:03:23 +0100

    quaternions start

---
+++ MatGraf1/Quaternions.cpp
@@ -0,0 +1,29 @@
+#include "Quaternions.h"
+
+Quaternions::Quaternions()
+{
+}
+
+Quaternions::Quaternions(float a, float b, float c, float d)
+{
+}
+
+Quaternions::Quaternions(float a, Vector& v)
+{
+}
+
+void Quaternions::operator+=(const Quaternions& q)
+{
+}
+
+void Quaternions::operator-=(const Quaternions& q)
+{
+}
+
+void Quaternions::operator*(const Quaternions& q)
+{
+}
+
+void Quaternions::operator/(const Quaternions& q)
+{
+}
---
+++ MatGraf1/Quaternions.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Vector.h"
+class Quaternions
+{
+public:
+	float a,b,c,d;
+	Quaternions();
+	Quaternions(float a, float b, float c, float d);
+	Quaternions(float a, Vector& v);
+
+	void operator+=(const Quaternions& q);
+	void operator-=(const Quaternions& q);
+	void operator*(const Quaternions& q);
+	void operator/(const Quaternions& q);
+
+};
